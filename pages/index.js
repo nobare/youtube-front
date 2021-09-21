c05921542d5778eedad3d6ea3e5fa1628274c314
nobare <vinifalconi@gmail.com>
2021-09-21 17:40:25 -0300

    Removing a LOG

--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,6 @@ import Layout from '../components/Layout/Layout'
 import { useEffect, useState } from 'react'
 
 const Home = (props) => {
-  
-/*   const [search, setSearch] = useState()
-  const [displayVideos, setDisplayVideos] = useState([]);
-  
-  const submitInput = async () => {
-    const response = await fetch('http://localhost:3003/search', {
-      method: 'POST',
-      body: JSON.stringify({ query: search }),
-      headers: {
-        'accept' : 'application/json',
-        'Content-Type' : 'application/json',
-      }
-    })
-
-    const data = await response.json();
-    const videos = data.map(({ snippet, id:{ videoId } }) => ({
-      title: snippet.title,
-      description: snippet.description,
-      id: videoId,
-    })) 
-    console.log(videos);
-  }
- */
   return (
     <>
     <SearchBar />
@@ -40,7 +17,6 @@ const Home = (props) => {
 export async function getStaticProps() {
   const res = await fetch('http://localhost:3003/mock')
   const data = await res.json()
-  //console.log(data.map(({id: {videoId}}) => videoId ))
 
   return {
     props: {
